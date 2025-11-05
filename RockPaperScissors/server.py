94fd398fd89436ef6be67b44a7bd3b46befa4096
Tmy175 <mydht1719@ut.edu.vn>
2025-11-05 12:30:27 +0700

    Xử lý vòng lặp chính (select & I/O)

--- RockPaperScissors/server.py
+++ RockPaperScissors/server.py
@@ -28,3 +28,31 @@ def main():
 
 if __name__ == "__main__":
     main()
+#Xử lý vòng lặp chính (select & I/O) (feature/socket-event-loop)
+while inputs:
+    inputfd, outputfd, exceptfd = select.select(inputs, output, inputs)
+
+    for fd in inputfd:
+        if fd is serverSocket:
+            clientConnection, clientAddress = fd.accept()
+
+            if roomCount < maxQueue:
+                status = 'ready'
+                inputs.append(clientConnection)
+
+                messageQueue[clientConnection] = queue.Queue()
+                messageQueue[clientConnection].put(status + str(roomCount))
+                output.append(clientConnection)
+
+                roomCount += 1
+
+                if roomCount == maxQueue:
+                    startGame = True
+
+            else:
+                status = 'queue'
+                clientQueue[clientConnection] = queue.Queue()
+                cQ.enqueue(clientConnection)
+                messageQueue[clientConnection] = queue.Queue()
+                messageQueue[clientConnection].put(status)
+                output.append(clientConnection)
