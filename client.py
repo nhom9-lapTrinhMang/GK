5bbd1d812106709476226e03d48a58409258fdf0
Trinm3468 <Trinm3468@ut.edu.vn>
2025-11-06 22:51:54 +0700

    Cập nhật logic game client

--- client.py
+++ client.py
@@ -31,3 +31,55 @@ if len(sys.argv) > 1:
             bufferSize = 1024
 else:
     usage()
+#  Logic game client 
+target = (address, port)
+
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientSocket.connect(target)
+
+status = clientSocket.recv(bufferSize).decode()
+player = '-1'
+
+if '0' in status:
+    player = '1'
+elif '1' in status:
+    player = '2'
+
+print(status)
+
+if 'queue' in status:
+    while status == 'queue':
+        print('The room is full, you have been added to the queue.')
+        status = clientSocket.recv(bufferSize).decode()
+    print(status)
+    print('You are now connected!')
+
+isPlaying = True
+
+while isPlaying:
+    clientInput = getinput()
+
+    if clientInput:
+        clientSocket.send((clientInput + str(player)).encode())
+        result = clientSocket.recv(bufferSize).decode()
+
+        if 'wait' in result:
+            print('Waiting for your opponent!')
+            result = clientSocket.recv(bufferSize).decode()
+            result = clientSocket.recv(bufferSize).decode()
+
+        if '0' in result:
+            print('The match was a draw!')
+        elif '1' in result and player == '1':
+            print('You won!')
+        elif '1' in result and player == '2':
+            print('You lost!')
+        elif '2' in result and player == '1':
+            print('You lost!')
+        elif '2' in result and player == '2':
+            print('You Won!')
+
+        print('Disconnecting to make room for other players. Thank you for playing SPEED-RPS!')
+
+        clientSocket.close()
+        isPlaying = False
